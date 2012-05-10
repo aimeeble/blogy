433efd4d3453e9f91e08ec30e9536c208ab6a72f
Aimee <aimee@aimeeble.com>
2012-05-09 23:18:43 -0700

    django project

---
+++ aimeeble/aimeeble/urls.py
@@ -0,0 +1,16 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'aimeeble.views.home', name='home'),
+    # url(r'^aimeeble/', include('aimeeble.foo.urls')),
+
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += staticfiles_urlpatterns()
