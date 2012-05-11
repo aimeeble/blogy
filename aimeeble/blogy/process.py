3048d0bc2e60cc63e81d4b9a5681d75a6bba335c
Aimee <aimee@aimeeble.com>
2012-05-10 23:51:47 -0700

    Cleanup static files.
    In the pre-save, delete the previous static file (if any) and also
    delete when deleting the entry.  Basically, stop leaving random HTML
    files lying around.

--- aimeeble/blogy/process.py
+++ aimeeble/blogy/process.py
@@ -1,16 +1,71 @@
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.template import Context
 from django.template import loader
+from django.utils import timezone
 
 from blogy.models import Entry
 from markdown import Markdown
 import os
 
+
+def get_static_filename(entry):
+   """Gets the absolute filename of the entry's static HTML file.
+
+   """
+   posted = timezone.localtime(entry.post)
+
+   filename = "%s.html" % (entry.slug)
+   path = os.path.join(settings.MEDIA_ROOT,
+                           "blog",
+                           "%4u" % (posted.year),
+                           "%02u" % (posted.month),
+                           "%02u" % (posted.day))
+   abs_filename = os.path.join(path, filename)
+
+   try:
+      os.makedirs(path)
+   except OSError, e:
+      pass
+
+   return abs_filename
+
+
+def delete_static_file(entry):
+   """Delete the HTML file associated with entry.
+
+   """
+   abs_filename = get_static_filename(entry)
+
+   print "Unlinking %s" % (abs_filename)
+   try:
+      os.unlink(abs_filename)
+   except OSError, e:
+      print "failure: %s" % (str(e))
+
+
+@receiver(pre_save, sender=Entry)
+def pre_save_handler(sender, instance, **kwargs):
+   """Cleans up generated files pre-save.
+
+   Pre-save, we delete the static HTML file if it already exists.  This does
+   two main things, 1) if the slug changes, this ensures the old file is
+   removed before we re-generate into the new slug file, and 2) handles hiding
+   a HTML file if the post-date has been changed to be in the future (invisible
+   until the date specified).
+
+   """
+   if not instance.id:
+      return
+   old = Entry.objects.get(pk=instance.id)
+   delete_static_file(old)
+
+
 @receiver(post_save, sender=Entry)
-def save_handler(sender, instance, **kwargs):
+def post_save_handler(sender, instance, **kwargs):
    """Generate the static HTML file for an entry.
 
    After the bookkeeping data is saved to the database, this will create the
@@ -19,6 +74,10 @@ def save_handler(sender, instance, **kwargs):
    then saved to a URL based upon the instance's slug.
 
    """
+   if not instance.posted():
+      print "not generating for future post on %s" % (str(instance.post))
+      return
+
    md = Markdown()
    post_html = md.convert(instance.markdown)
 
@@ -29,21 +88,12 @@ def save_handler(sender, instance, **kwargs):
    })
    full_html = t.render(c)
 
-   filename = "%s.html" % (instance.slug)
-   path = os.path.join(settings.MEDIA_ROOT,
-                           "blog",
-                           "%4u" % (instance.post.year),
-                           "%02u" % (instance.post.month),
-                           "%02u" % (instance.post.day))
-   abs_filename = os.path.join(path, filename)
+   abs_filename = get_static_filename(instance)
 
-   try:
-      os.makedirs(path)
-   except OSError, e:
-      pass
    with open(abs_filename, "w+") as f:
       f.write(full_html)
    print "saving %s to %s ->\n%s" % (instance.title, abs_filename, full_html)
+
 
 @receiver(pre_delete, sender=Entry)
 def delete_handler(sender, instance, **kwargs):
@@ -53,7 +103,7 @@ def delete_handler(sender, instance, **kwargs):
    delete fail, re-saving we re-generate the file we delete here.
 
    """
-   print "deleting %s" % (instance.title)
+   delete_static_file(instance)
 
 
 def setup_signals():
