4a763fe50e9ee2d5753e2f1f3ccd99a77631df8b
Aimee <aimee@aimeeble.com>
2012-06-17 16:55:58 -0700

    switch to redis as celery backend

--- aimeeble/aimeeble/settings.py
+++ aimeeble/aimeeble/settings.py
@@ -14,9 +14,16 @@ CELERYBEAT_SCHEDULE = {
    "publish-articles": {
       "task": "blogy.tasks.process_pending_posts_task",
       "schedule": timedelta(minutes=1),
-      "args": None,
+      "args": tuple(),
    },
 }
+CELERYBEAT_SCHEDULER = "djcelery.schedulers.DatabaseScheduler"
+
+BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis"
+CELERY_REDIS_HOST = "localhost"
+CELERY_REDIS_PORT = 6379
+CELERY_REDIS_DB = 0
 
 
 # Django settings for aimeeble project.
