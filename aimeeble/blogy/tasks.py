c4dbc591eec024152a2f0999657b76aebbdaa78d
Aimee <aimee@aimeeble.com>
2012-06-17 19:05:08 -0700

    split process task into two parts

--- aimeeble/blogy/tasks.py
+++ aimeeble/blogy/tasks.py
@@ -8,28 +8,58 @@ import os
 
 
 @task(ignore_results=True)
-def process_pending_posts_task(entrypk=None):
+def process_pending_posts_task():
+   """Task to process pending but not ready entries.
+
+   This will generate all posts which should but have not yet been processed.
+   """
+   print "Processing pending..."
+
+   now = timezone.now()
+   raw_entries = Entry.objects.filter(rendered=False, post__lte=now)
+   todo_tags = []
+
+   if not raw_entries:
+      print "DEBUG: none pending"
+      return
+
+   for entry in raw_entries:
+      generate_single_post(entry.id)
+      todo_tags += [tag.name for tag in entry.tags.all()]
+
+   generate_main_index_task()
+   if todo_tags:
+      todo_tags = set(todo_tags)
+      generate_tag_index_task(todo_tags)
+
+
+@task(ignore_results=True)
+def generate_single_post(entrypk):
    """Task to process posts.
 
    This is where all processing of post markdown to HTML is kicked off from.
-   If entrypk is specified, we will only generate that entry.  If it is None,
-   we will generate all posts which should but have not yet been processed.
    """
-   print "Processing..."
 
-   if entrypk:
-      entry = Entry.objects.get(pk=entrypk)
-      if not entry:
-         raise Exception("invalid pk %s" % entrypk)
-      entries = [StaticEntry(entry)]
-   else:
-      now = timezone.now()
-      raw_entries = Entry.objects.filter(finished=True, post__lte=now)
-      entries = [StaticEntry(x) for x in raw_entries]
+   raw_entry = Entry.objects.get(pk=entrypk)
+   if not raw_entry:
+      raise Exception("invalid pk %s" % entrypk)
 
-   for entry in entries:
-      entry.generate_html()
+   if raw_entry.rendered:
+      print "DEBUG: skipping previously rendered entry"
+      return
+
+   print "rendering entry %s" % (raw_entry.slug)
+   entry = StaticEntry(raw_entry)
+
+   # Generate the private guid-based HTML file.
+   entry.generate_html()
+
+   # Link the publically visible file only if ready...
+   if raw_entry.post <= timezone.now() and raw_entry.finished:
       entry.symlink()
+
+      raw_entry.rendered = True
+      raw_entry.save()
 
 
 @task(ignore_results=True)
@@ -37,7 +67,6 @@ def generate_main_index_task():
    """Task to re-generate the main index.
 
    """
-   print "Generating indexes..."
    idx = StaticIndex()
    idx.generate_html()
 
@@ -51,7 +80,6 @@ def generate_tag_index_task(tags=None):
    """
    if not tags:
       tags = map(lambda x: x.name, Tag.objects.all())
-   print "Generating indexes for %s..." % (", ".join(tags))
 
    for tag in tags:
       idx = StaticIndex(tag)
